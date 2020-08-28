8e1548121d1e062c678cda546948823beb0b94a4
jslee7420 <jeongsoo.lee7420@gmail.com>
2020-08-28 21:26:30 +0900

    UPDATE accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, NativeLanguage, StudyingLanguage
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
+admin.site.register(NativeLanguage)
+admin.site.register(StudyingLanguage)
--- accounts/models.py
+++ accounts/models.py
@@ -3,79 +3,25 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    country=models.CharField(max_length=100)
     phone_number=models.CharField(max_length=11)
     major=models.CharField(max_length=100)
-    introduction=models.CharField(max_length=100,blank="True")
-    online_letter=models.BooleanField()
-    is_korean=models.BooleanField()
-    is_foreigner=models.BooleanField()
+    introduction=models.CharField(max_length=200,blank=True)
+    online_letter=models.BooleanField(default=True)
 
-class KoreanUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    LANGUAGE_CHOICES = [
-        ('보통화', 'MADARIN CHINESE'),
-        ('스페인어', 'SPANISH'),
-        ('영어', 'ENGLISH'),
-        ('힌디어', 'HINDI'),
-        ('뱅골어', 'BENGALI'),
-        ('포르투갈어', 'PORTUGUESE'),
-        ('러시아어', 'RUSSIAN'),
-        ('일본어', 'JAPANESE'),
-        ('란다어', 'WESTERN PUNJABI'),
-        ('마라티어', 'MARATHI'),
-        ('텔루구어', 'TELUGU'),
-        ('우어', 'WU CHINESE'),
-        ('터키어', 'TURKISH'),
-        ('불어', 'FRENCH'),
-        ('독어', 'GERMAN'),
-        ('베트남어', 'VIETNAMESE'),
-        ('타밀어', 'TAMIL'),
-        ('웨어', 'YUE CHINESE'),
-        ('우르두어', 'URDU'),
-        ('자와어', 'JAVANESE'),
-    ]
-    language = models.CharField(choices=LANGUAGE_CHOICES, blank=True, max_length=100)
 
+
+class NativeLanguage(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    language=models.CharField(max_length=100)
+
+
+class StudyingLanguage(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    language=models.CharField(max_length=100)
     LEVEL_CHOICES=[
-        ('상','ADVANCED'),
-        ('중','INTERMIDIATE'),
-        ('하','BASIC'),
-    ]
-    language_level=models.CharField(choices=LEVEL_CHOICES, blank=True, max_length=100)
-
-
-
-
-class ForeignUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    country=models.CharField(max_length=100)
-    LANGUAGE_CHOICES = [
-        ('MADARIN CHINESE', 'MADARIN CHINESE'),
-        ('SPANISH', 'SPANISH'),
-        ('ENGLISH', 'ENGLISH'),
-        ('HINDI', 'HINDI'),
-        ('BENGALI', 'BENGALI'),
-        ('PORTUGUESE', 'PORTUGUESE'),
-        ('RUSSIAN', 'RUSSIAN'),
-        ('JAPANESE', 'JAPANESE'),
-        ('WESTERN PUNJABI', 'WESTERN PUNJABI'),
-        ('MARATHI', 'MARATHI'),
-        ('TELUGU', 'TELUGU'),
-        ('WU CHINESE', 'WU CHINESE'),
-        ('TURKISH', 'TURKISH'),
-        ('FRENCH', 'FRENCH'),
-        ('GERMAN', 'GERMAN'),
-        ('VIETNAMESE', 'VIETNAMESE'),
-        ('TAMIL', 'TAMIL'),
-        ('YUE CHINESE', 'YUE CHINESE'),
-        ('URDU', 'URDU'),
-        ('JAVANESE', 'JAVANESE'),
-    ]
-    native_language=models.CharField(choices=LANGUAGE_CHOICES, max_length=100)
-
-    LEVEL_CHOICES=[
-        ('ADVANCED','ADVANCED'),
-        ('INTERMIDIATE','INTERMIDIATE'),
         ('BASIC','BASIC'),
+        ('INTERMIDIATE','INTERMIDIATE'),
+        ('ADVANCED','ADVANCED'),
     ]
-    korean_level=models.CharField(choices=LEVEL_CHOICES, max_length=100)
+    level=models.CharField(choices=LEVEL_CHOICES,default='BASIC', max_length=50)
